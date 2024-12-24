f12fe4d8f729e522fe8453ae2617024ee667f6c5
Zirins <BensonLin80169@gmail.com>
2024-12-24 00:02:22 -0800

    finished navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // For navigation
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className="navBar">
-            <div className="logo">My Portfolio</div>
-            <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+        <nav className="flex items-center justify-between p-6 bg-gray-800 text-white shadow-md">
+            {/* Updated Name */}
+            <div className="text-4xl font-extrabold tracking-wide">Benson Lin</div>
+
+            <ul className="flex space-x-6 text-lg font-medium">
+                <li><Link className="hover:text-blue-400" to="/">Home</Link></li>
+                <li><Link className="hover:text-blue-400" to="/about">About</Link></li>
+                <li><Link className="hover:text-blue-400" to="/projects">Projects</Link></li>
+                <li><Link className="hover:text-blue-400" to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
